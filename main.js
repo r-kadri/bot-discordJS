e2925fba4ea654c6c1f3927d80ec682dc09cf08f
Ryan Kadri <ryankadri.dev@gmail.com>
2023-01-31 18:15:24 +0100

    first run

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const { Client, GatewayIntentBits } = require('discord.js');
+const BOT_NAME = 'Gummy Bee';
+
+// SECRET TOKEN (ignored file)
+const LOGIN_TOKEN = require('./token.json');
+
+const CLIENT = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+CLIENT.on('ready', () => {
+    console.log(BOT_NAME + ' is ready !');
+});
+
+CLIENT.login(LOGIN_TOKEN.token);
